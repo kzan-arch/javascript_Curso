31b27464901de059a02b7ac24dac5dd2410e6d17
Kzan-arch <vinikauan093542@gmail.com>
2025-11-25 14:01:18 -0300

    ativi

---
+++ filter1.js
@@ -0,0 +1,12 @@
+let alunos = [ "Ana-15", "Carlos-14", "Maria-15", "João-16",
+    "Beatriz-15", "Pedro-13", "Luiza-15", "Ricardo-14"];
+
+let = alunos.filter((alunos) => {
+
+    return alunos == 15;
+
+});
+
+console.log(alunos);
+console.log(alunos)
+    
---
+++ filter2.js
@@ -0,0 +1,9 @@
+let notas = [14, 15, 16, 15, 13, 15, 17, 15, 14, 15]
+ 
+let aprovados = notas.filter((notas) =>{
+    return notas == 15;
+});
+ 
+console.log(notas);
+ 
+console.log(aprovados);
